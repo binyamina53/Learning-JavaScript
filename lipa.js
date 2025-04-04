523888b805664a1d0cf1b20d7cafb9f7c5a62be5
binyamina53 <553glr@gmail.com>
2025-04-04 16:02:38 +0300

    Add files via upload

---
+++ lipa.js
@@ -0,0 +1,11 @@
+let Victory_String;
+document.getElementById("bolg").onclick = function () {
+    Victory_String = "You defeated the Lipi's";
+    document.getElementById("lg0").innerHTML = "well done, you saved the world!";
+    document.getElementById("lg1").innerHTML = Victory_String;
+    document.getElementById("lg2").innerHTML = Victory_String;
+    document.getElementById("lg3").innerHTML = Victory_String;
+    document.getElementById("lg4").innerHTML = Victory_String;
+    document.getElementById("lg5").innerHTML = Victory_String;
+    document.getElementById("bolg").innerHTML = "you ween!";
+}
